331b7e12b88c04dc12492fea64c4e05e7e69ce7b
azazzze1 <evshushkov@stud.eltech.ru>
2023-11-23 02:44:21 +0300

    CW: ALL GARBAGE DELETE

--- LB4/my-angular/src/app/app.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-import { CookieService } from 'ngx-cookie-service';
-
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-})
-export class AppComponent {}
